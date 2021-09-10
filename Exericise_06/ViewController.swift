aa0347cb5f58079f8a86d392717211464e0072cf
HiroyukiShiraishi <hiro19900402@gmail.com>
2021-09-10 09:08:19 +0900

    課題-006

--- Exericise_06/ViewController.swift
+++ Exericise_06/ViewController.swift
@@ -8,81 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private var questionLabel: UILabel!
+    @IBOutlet private var slider: UISlider!
+    private var sliderNum = 0
+    private var questionNumber = 0
 
-    @IBOutlet var numberLabel: UILabel!
-    
-    
-    @IBOutlet var sliderObj: UISlider!
-    
-    private var sliderNum = Int()
-    private var number = Int()
-    
-    private var arc4random:UInt32?
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        randomsetUp()
-       
-     
-        // Do any additional setup after loading the view.
+        questionNumberSetUp()
     }
-    
-    //MARK:METHOds
 
-    @IBAction func slider(_ sender: UISlider) {
-        sliderNum = Int(sender.value)
-        
+    // MARK: METHOds
+
+    @IBAction private func judgementButton(_ sender: Any) {
+        judgment()
     }
-    
-    @IBAction func judgementButton(_ sender: Any) {
-        _result()
-        
-    }
-    
-    private func alert(title:String = "結果",message:String,okTitle:String = "再挑戦"){
-        
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        let okAtion = UIAlertAction(title: okTitle, style: .default) { okAction in
-            
-            self.randomsetUp()
+
+    private func alert(alertMessage: String) {
+        let alert = UIAlertController(title: "結果", message: alertMessage, preferredStyle: .alert)
+
+        let okAtion = UIAlertAction(title: "再挑戦", style: .default) { _ in
+            self.questionNumberSetUp()
         }
         alert.addAction(okAtion)
         present(alert, animated: true, completion: nil)
-        
     }
-    
-    private func _result(){
-        if sliderNum == number{
-            alert(message: "アタリ!\nあなたの値\(sliderNum)")
-        }else{
-            alert(message: "ハズレ!\nあなたの値\(sliderNum)")
+
+    private func judgment() {
+        self.sliderNum = Int(slider.value)
+        if sliderNum == questionNumber {
+            alert(alertMessage: "アタリ!\nあなたの値\(sliderNum)")
+        } else {
+            alert(alertMessage: "ハズレ!\nあなたの値\(sliderNum)")
         }
     }
-    
-    private func randomsetUp(){
-        let ranNum = Int.random(in: 1...100)
-        numberLabel.text = String(ranNum)
-        number = ranNum
-        sliderObj.value = 50.0
-    }
-    
-    //arc4random_uniformVer
-    private func ranNumSetup(){
-        for _ in 1...100{
-           arc4random = arc4random_uniform(100)
-        }
-        let nweArc4random = Int(arc4random!)
-        numberLabel.text = String(nweArc4random)
-        number = Int(arc4random!)
-        sliderObj.value = 50.0
-        print(number)
 
+    private func questionNumberSetUp() {
+        let randomNumber = Int.random(in: 1...100)
+        questionNumber = randomNumber
+        updateLabel()
+        slider.value = 50.0
     }
-    
-    
-
+    private func updateLabel() {
+        questionLabel.text = String(questionNumber)
+    }
 }
-
